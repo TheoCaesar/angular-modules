7bf59cf43fbd687aeaf7d8e1b61776ee02d1d32d
Theophilus Ofoe Caesar <theophilusofoe.caesar@solv.com.gh>
2024-08-26 18:47:35 +0000

    adding feature routes to feature modules

---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipesResolverService } from "./recipes-resolver.service";
+
+export const recipeRoutes:Routes = [
+  { path: '', component: RecipeStartComponent },
+  { path: 'new', component: RecipeEditComponent },
+  {
+    path: ':id',
+    component: RecipeDetailComponent,
+    resolve: [RecipesResolverService]
+  },
+  {
+    path: ':id/edit',
+    component: RecipeEditComponent,
+    resolve: [RecipesResolverService]
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(recipeRoutes)],
+  exports: [RouterModule]
+})
+export class RecipeRoutingModule {}
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { RecipesComponent } from './recipes.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeRoutingModule } from './recipes-routing.module';
 
 @NgModule({
   declarations: [
@@ -19,7 +19,14 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
     RecipeStartComponent,
     RecipeEditComponent
   ],
-  imports: [RouterModule, CommonModule, ReactiveFormsModule],
+  //provide angular directives and keywords for feature components
+  imports: [
+    RouterModule,
+    CommonModule,
+    ReactiveFormsModule,
+    RecipeRoutingModule,
+  ],
+  //allow access to components in app module
   exports: [
     RecipesComponent,
     RecipeListComponent,
